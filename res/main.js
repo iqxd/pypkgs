d4cff28176b891dd2ecbbc55b7f771d86e0b1eb5
iqxd <327614680@qq.com>
2020-04-12 21:08:01 +0800

    mark updatable packages

--- res/main.js
+++ res/main.js
@@ -1,10 +1,31 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
-(function () {
-   const vscode = acquireVsCodeApi();
+function versionStringCompare(preVersion = '', lastVersion = '') {
+    var sources = preVersion.split('.');
+    var dests = lastVersion.split('.');
+    var maxL = Math.max(sources.length, dests.length);
+    var result = 0;
+    for (let i = 0; i < maxL; i++) {
+        let preValue = sources.length > i ? sources[i] : 0;
+        let preNum = isNaN(Number(preValue)) ? preValue.charCodeAt() : Number(preValue);
+        let lastValue = dests.length > i ? dests[i] : 0;
+        let lastNum = isNaN(Number(lastValue)) ? lastValue.charCodeAt() : Number(lastValue);
+        if (preNum < lastNum) {
+            result = -1;
+            break;
+        } else if (preNum > lastNum) {
+            result = 1;
+            break;
+        }
+    }
+    return result;
+}
 
-   const table = document.getElementById('content');
-    
+(function () {
+    const vscode = acquireVsCodeApi();
+
+    const table = document.getElementById('content');
+
     // Handle messages sent from the extension to the webview
     window.addEventListener('message', event => {
         const message = event.data; // The json data that the extension sent
@@ -18,6 +39,12 @@
             }
             dropdown += '</select>';
             row.cells[8].innerHTML = dropdown;
+            
+            const currentVer = row.cells[1].innerHTML;
+            const latestVer = vers[0];
+            if (versionStringCompare(currentVer, latestVer) === -1) {
+                row.cells[1].innerHTML = '▲ ' + currentVer;
+            }
         }
     });
 }());
