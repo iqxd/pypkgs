e6421082536b9efab343968fb29eab6410d94d33
iqxd <327614680@qq.com>
2020-04-12 20:22:26 +0800

    remove unuseful message

--- res/main.js
+++ res/main.js
@@ -10,16 +10,7 @@
         const message = event.data; // The json data that the extension sent
         const dataType = message.type;
         let row = table.rows[message.row];
-        if (dataType == 'details') { 
-            row.cells[0].innerHTML = message.name;
-            row.cells[1].innerHTML = message.version;
-            row.cells[2].innerHTML = message.summary;
-            row.cells[3].innerHTML = message.homepage;
-            row.cells[4].innerHTML = message.author;
-            row.cells[5].innerHTML = message.authoremail;
-            row.cells[6].innerHTML = message.license;
-            row.cells[7].innerHTML = message.location;
-        } else if (dataType == 'vers') {
+        if (dataType == 'vers') {
             let vers = JSON.parse(message.allvers);
             let dropdown = '<select style="width:90px;">';
             for (let ver of vers) {
