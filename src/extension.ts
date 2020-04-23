1e284550417123dc449db4bdd8ffdb66692f385c
iqxd <327614680@qq.com>
2020-04-23 19:48:50 +0800

    add statusbar item to run command

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as child_process from 'child_process';
 import { PackagesView } from './packagesView';
 import { PythonManager } from './pythonManager';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pythonpackages" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.showpypkgs', async () => {
-		// The code you place here will be executed every time your command is executed
+	let showPyPkgs = vscode.commands.registerCommand('extension.showpypkgs', async () => {
+	
 		const config = vscode.workspace.getConfiguration();
 		let currentPyPath = config.get<string>("python.pythonPath");
 		if (currentPyPath == undefined) {
@@ -38,9 +30,16 @@ export async function activate(context: vscode.ExtensionContext) {
 		PackagesView.createOrShow(context.extensionPath, python);
 
 	});
+	
+	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	statusBarItem.text = 'showpypkgs';
+	statusBarItem.command = 'extension.showpypkgs';
+	
+	context.subscriptions.push(showPyPkgs);
+	context.subscriptions.push(statusBarItem);
 
-	context.subscriptions.push(disposable);
-}
+	statusBarItem.show();
+}  
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
